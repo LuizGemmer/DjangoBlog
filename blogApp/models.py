3b2595f562033a16f3f126cb72335bf5bee85319
Luiz Gemmer <luiz.o.gemmer@gmail.com>
2023-05-20 12:07:25 -0300

    started dealing with models (post and media models)

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Media(models.Model):
+    file_path = models.FileField("media", upload_to="post_media/%y/%m/%d/")
+    type = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.file_path.name
+    
+
+class Post(models.Model):
+    text = models.TextField("post")
+    media = models.ManyToManyField(Media, blank=True)
+
+    date_posted = models.DateTimeField("posted", auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField("last modified", auto_now=True, editable=False)
+
+
+    in_response_to = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, editable=False)
+    #author = models.ForeignKey()
+
+    def __str__(self):
+        return f"{self.text[:20]} by not implemented"
+
+
+
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,9 +1,15 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 
-# Create your views here.
-#class HomeView(generic.ListView):
-#    template_name='blogApp/home.html'
+from .models import Post
 
-def home(request):
-    return render(request, template_name='blogApp/home.html')
+# Create your views here.
+class HomeView(generic.ListView):
+    model=Post
+    template_name='blogApp/home.html'
+    context_object_name="posts"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Post.objects.all()
